e27721e24f6e916fa7a1462eec73a24f68edb562
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2022-09-06 23:35:31 +0300

    fix

---
+++ src/js/station.js
@@ -0,0 +1,7 @@
+const refs = {
+  station: document.querySelector('.js-station'),
+  //   langSwitch: document.querySelector('.user_profile_lang'),
+  //   sidebarSection: document.querySelector('.skills'),
+  //   sidebarTitle: document.querySelector('.sidebar-title'),
+};
+refs.mainSection.innerHTML = `<source src="https://hls-02-europaplus.emgsound.ru/11/128/playlist.m3u8" type="application/x-mpegURL" />`;
