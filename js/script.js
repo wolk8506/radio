151e4e0e9e91e13e44922d4d3cbb79e75ae295c9
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2023-05-24 19:41:47 +0300

    обновление данных о погоде

--- js/script.js
+++ js/script.js
@@ -18,9 +18,26 @@ fetchUsers().then((data) => weatherF(data));
 function weatherF(data) {
   weather.innerHTML = `
     <ul>
-        <li>${data.location.name}</li>
-        <li>temp_c: ${data.current.temp_c}</li>
-        <li>condition: ${data.current.condition.text}</li>
+        <li>Страна: ${data.location.country}</li>
+        <li>Город: ${data.location.name}</li>
+        <li>Таймзона: ${data.location.tz_id}</li>
+        <li> <img src='${data.current.condition.icon}'></li>
+        <li>Погодные условия: ${data.current.condition.text}</li>
+        <li>Температура: ${data.current.temp_c}°C</li>
+        
+        <li>Местное время, когда данные в реальном времени были обновлены: ${data.current.last_updated}</li>
+        <li>Скорость ветра в километрах в час: ${data.current.wind_kph}</li>
+        <li>Направление ветра в градусах: ${data.current.wind_degree}</li>
+        <li>Направление ветра в виде компаса с 16 точками: ${data.current.wind_dir}</li>
+        <li>УФ-индекс: ${data.current.uv}</li>
+        <li>Давление в миллибарах: ${data.current.gust_kph}</li>
+        <li>Порывы ветра в километрах в час: ${data.current.pressure_mb}</li>
+        <li>Количество осадков в миллиметрах: ${data.current.precip_mm}</li>
+        <li>Влажность в процентах: ${data.current.humidity}</li>
+        <li>Облачность в процентах: ${data.current.cloud}</li>
+        <li>По ощущениям температура как по Цельсию: ${data.current.feelslike_c}</li>
+        <li>Видимость в километре: ${data.current.vis_km}</li>
+
     </ul>
     `;
 }
